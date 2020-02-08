0b929d441e13f03919e2b419550a3fdeabd648ca
Amit Kumar Giri <allyamit@gmail.com>
2020-02-08 13:40:50 +0530

    User interface change. Calnder type.

--- app/src/main/java/com/trulloy/bfunx/MainActivity.java
+++ app/src/main/java/com/trulloy/bfunx/MainActivity.java
@@ -1,15 +1,18 @@
 package com.trulloy.bfunx;
 
+import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,18 +20,14 @@ public class MainActivity extends AppCompatActivity {
     ImageView answerImage;
     String answer;
     Button closeBtn, predict;
+    DatePickerDialog datePicker;
+    EditText dateEdtText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final Spinner monthSpinner = findViewById(R.id.monthSpinner);
-        ArrayAdapter<String> monthSpinnerAdapter = new ArrayAdapter<>(MainActivity.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.months));
-        monthSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        monthSpinner.setAdapter(monthSpinnerAdapter);
         closeBtn = findViewById(R.id.closeBtn);
         closeBtn.setOnClickListener(new View.OnClickListener(){
            @Override
@@ -37,9 +36,32 @@ public class MainActivity extends AppCompatActivity {
                System.exit(0);
            }
         });
+
+        dateEdtText = findViewById(R.id.dateEdtTxt);
+        dateEdtText.setInputType(InputType.TYPE_NULL);
+        dateEdtText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                datePicker = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                dateEdtText.setText(dayOfMonth + " / " + (monthOfYear + 1) + " / " + year);
+                            }
+                        }, year, month, day);
+                datePicker.show();
+            }
+        });
+
         predict = findViewById(R.id.predictBtn);
         answerTxtView = findViewById(R.id.answerTxtView);
         answerImage = findViewById(R.id.answerImg);
+      
         predict.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -52,12 +74,18 @@ public class MainActivity extends AppCompatActivity {
                     age = 0;
                     // TODO - throw error and show dialog box
                 }
-                int month = (int) monthSpinner.getSelectedItemId();
+
+                int month = -1;
+                if (datePicker != null && datePicker.getDatePicker() != null) {
+                    month = datePicker.getDatePicker().getMonth();
+                }
                 if (age < 18 || age > 45) {
-                    month = -1;
-                    age = 0;
                     answerTxtView.setTextColor(Color.RED);
                     answer = "Please input the age between 18 and 45.";
+                    answerImage.setImageResource(R.drawable.sorryicon_100x85);
+                } else if (month < 0) {
+                    answerTxtView.setTextColor(Color.RED);
+                    answer = "Please select date first.";
                     answerImage.setImageResource(R.drawable.sorryicon_100x85);
                 } else {
                     answerTxtView.setTextColor(Color.rgb(0,150,0));
