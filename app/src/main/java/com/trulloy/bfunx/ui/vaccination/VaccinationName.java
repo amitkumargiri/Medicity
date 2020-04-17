2094e00bb03f352c42ceb2f940b394345c9a0b3c
Amit Kumar Giri <allyamit@gmail.com>
2020-04-17 15:07:22 +0530

    Added vaccine names and modified vaccine list.

---
+++ app/src/main/java/com/trulloy/bfunx/ui/vaccination/VaccinationName.java
@@ -0,0 +1,64 @@
+package com.trulloy.bfunx.ui.vaccination;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class VaccinationName {
+
+    final public static List<String> VACCINES_BIRTH = Arrays.asList(
+            "Birth: Bacillus Calmette–Guérin (BCG)",
+            "Birth: Oral Polio Vaccine (OPV 0)",
+            "Birth: Hepatitis B (HB 1)"
+    );
+    final public static List<String> VACCINES_6WEEKS = Arrays.asList(
+            "6 Weeks: Diptheria, Tetanus and Pertussis vaccine (DTP 1)",
+            "6 Weeks: Inactivated Polio Vaccine (IPV 1)",
+            "6 Weeks: Hepatitis B  (HB 2)",
+            "6 Weeks: Haemophilus Influenzae Type B (HiB 1)",
+            "6 Weeks: Rotavirus 1",
+            "6 Weeks: Pneumococcal Conjugate Vaccine (PCV 1)"
+    );
+    final public static List<String> VACCINES_10WEEKS = Arrays.asList(
+            "10 Weeks: Diptheria, Tetanus and Pertussis vaccine (DTP 2)",
+            "10 Weeks: Haemophilus Influenzae Type B (HiB 2)",
+            "10 Weeks: Inactivated Polio Vaccine (IPV 2)",
+            "10 Weeks: Hepatitis B (HB 3)",
+            "10 Weeks: Rotavirus 2",
+            "10 Weeks: Pneumococcal Conjugate Vaccine (PCV 2)"
+    );
+    final public static List<String> VACCINES_14WEEKS = Arrays.asList(
+            "14 Weeks: Diptheria, Tetanus and Pertussis Vaccine (DTP 3)",
+            "14 Weeks: Haemophilus Influenzae Type B (HiB 3)",
+            "14 Weeks: Inactivated Polio Vaccine (IPV 3)",
+            "14 Weeks: Hepatitis B  (HB 4)",
+            "14 Weeks: Rotavirus 3",
+            "14 Weeks: Pneumococcal Conjugate Vaccine (PCV 3)"
+    );
+    final public static List<String> VACCINES_691215MONTHS = Arrays.asList(
+            "6 Months: Typhoid Conjugate Vaccine (TCV)",
+            "9 Months: Measles, Mumps, and Rubella (MMR 1)",
+            "12 Months: Hepatitis A (Hep A1)",
+            "12 Months: Influenza (Yearly)",
+            "13 Months: Influenza (Yearly)",
+            "15 Months: Measles, Mumps, and Rubella (MMR 2)",
+            "15 Months: Varicella 1",
+            "15 Months: Influenza (Yearly)",
+            "15 Months: PCV Booster 1"
+    );
+    final public static List<String> VACCINES_1618MONTHS = Arrays.asList(
+            "16 to 18 Months: Diphtheria, Perussis, and Tetanus (DTP B1)",
+            "16 to 18 Months: Inactivated polio vaccine (IPV B1)",
+            "16 to 18 Months: Haemophilus influenzae type B (Hib B1)",
+            "16 to 18 Months: Hepatitis A (Hep – A2)"
+    );
+    final public static List<String> VACCINES_2461012YEARS = Arrays.asList(
+            "Booster of Typhoid",
+            "Conjugate Vaccine",
+            "Diphtheria, Perussis, and Tetanus (DTwP B2/DTaP B2)",
+            "Oral polio vaccine (OPV 3)",
+            "Varicella 2",
+            "Measles, Mumps, and Rubella (MMR 3)",
+            "Tdap/Td",
+            "Human Papilloma Virus (HPV)"
+    );
+}
